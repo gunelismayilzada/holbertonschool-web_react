37daee62c6867a33b1fcfe8abf04550ab32aa796
gunelismayilzada <gunelismayilzad7@gmail.com>
2026-03-11 14:09:16 +0400

    typeScript

---
+++ TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,11 @@
+import { RowID, RowElement } from "./interface";
+
+export function insertRow(row: RowElement): RowID;
+
+export function deleteRow(rowId: RowID): void;
+
+export function updateRow(rowId: RowID, row: RowElement): RowID;export const row: RowElement = {
+  firstName: "Guillaume",
+  lastName: "Salva"
+};
+
